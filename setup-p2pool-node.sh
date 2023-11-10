89ca19ed98543d464469f03933515e5650dd3246
Gary Benson <gary@gbenson.net>
2023-11-10 22:23:48 +0000

    Run rpc-proxy/update-passwords.sh at p2pool.service start

--- setup-p2pool-node.sh
+++ setup-p2pool-node.sh
@@ -104,6 +104,7 @@ After=docker.service
 
 [Service]
 Type=simple
+ExecStartPre=-/bin/sh /opt/monero-node/rpc-proxy/update-passwords.sh
 ExecStart=docker compose -f /opt/monero-node/docker-compose.yml up
 ExecStop=docker compose -f /opt/monero-node/docker-compose.yml stop
 Restart=always
