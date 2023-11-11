d5226299aae9a338251595981a535534009ce658
Gary Benson <gary@gbenson.net>
2023-11-11 01:03:27 +0000

    Scale up!

--- miners.tf
+++ miners.tf
@@ -3,8 +3,15 @@ resource "openstack_networking_secgroup_v2" "xmrig" {
   description = "Security group for XMRig nodes"
 }
 
-resource "openstack_compute_instance_v2" "miner1" {
-  name            = "miner1"
+variable "num_miners" {
+  description = "Number of miner instances to provision"
+  type        = number
+  default     = 2
+}
+
+resource "openstack_compute_instance_v2" "miner" {
+  count           = var.num_miners
+  name            = "miner${count.index + 1}"
   key_pair        = openstack_compute_keypair_v2.keypair.id
   security_groups = ["default", "xmrig"]
   flavor_name     = "gp1.lightspeed"
@@ -12,8 +19,9 @@ resource "openstack_compute_instance_v2" "miner1" {
   user_data       = file("setup-miner.sh")
 }
 
-resource "dreamhost_dns_record" "miner1" {
-  record = "miner1.gbenson.net"
+resource "dreamhost_dns_record" "miner" {
+  count  = var.num_miners
+  record = "miner${count.index + 1}.gbenson.net"
   type   = "A"
-  value  = openstack_compute_instance_v2.miner1.access_ip_v4
+  value  = openstack_compute_instance_v2.miner[count.index].access_ip_v4
 }
