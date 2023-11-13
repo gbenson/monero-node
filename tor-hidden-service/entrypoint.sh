479f2d5ba063fd98f5c1c6db33ee274a98ef25b5
Gary Benson <gary@gbenson.net>
2023-11-13 19:59:46 +0000

    Make p2pool-tor container generic

--- tor-hidden-service/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-chown debian-tor:debian-tor /var/lib/tor
-chmod 02700 /var/lib/tor
---
+++ tor-node/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+if [ $# -gt 1 ]; then
+  echo 1>&2 "docker run gbenson/tor-node [[HOST:]PORT]"
+  exit 1
+fi
+
+unset host port
+if [ $# -eq 1 ]; then
+  port=$(echo "$1" | sed 's/.*://')
+  if [ "$port" != "$1" ]; then
+    host=$(echo "$1" | sed "s/:$port\$//")
+  fi
+fi
+echo "HOST=$host"
+echo "PORT=$port"
+
+if [ -n "$port" ]; then
+  f=/etc/tor/torrc
+  grep ^HiddenServicePort $f && exit 1  # crowbar
+  sed -e '0,/^#HiddenServicePort/{s/^#\(HiddenService\)/\1/}' \
+      -e '/^HiddenServicePort/{s/80/@@PORT@@/g}' \
+      -i $f
+  if [ -n "$host" ]; then
+    sed -i "/^HiddenServicePort/{s/127\.0\.0\.1/@@HOST@@/}" $f
+  fi
+  sed -e "/^HiddenServicePort/{s/@@HOST@@/$host/; s/@@PORT@@/$port/g}" \
+      -i $f
+  unset f
+fi
+
+unset host port
+
+chown debian-tor:debian-tor /var/lib/tor
+chmod 02700 /var/lib/tor
