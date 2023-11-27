2dcf14a6d8615ab9e3ac3131f981321b85effb57
Gary Benson <gary@gbenson.net>
2023-11-27 20:13:56 +0000

    Create miner launch templates

--- modules/aws/miners/miner.tf
+++ modules/aws/miners/miner.tf
@@ -1,9 +1,11 @@
-data "aws_ami" "amazon_linux_x86_64" {
+data "aws_ami" "amazon_linux" {
+  for_each = toset(["x86_64", "arm64"])
+
   most_recent = true
   owners      = ["amazon"]
   filter {
     name   = "architecture"
-    values = ["x86_64"]
+    values = [each.key]
   }
   filter {
     name   = "name"
@@ -11,20 +13,41 @@ data "aws_ami" "amazon_linux_x86_64" {
   }
 }
 
-resource "aws_instance" "miner" {
-  count = 0
-  ami           = data.aws_ami.amazon_linux_x86_64.id
-  instance_type = "c6i.large"
+resource "aws_launch_template" "miner" {
+  for_each = data.aws_ami.amazon_linux
+  name     = "${each.value.architecture}-miner"
+  image_id = each.value.id
+
+  instance_requirements {
+    vcpu_count {
+      min = 1
+    }
+    memory_mib {
+      min = "4096"
+    }
+    burstable_performance = "excluded"
+  }
+
   instance_market_options {
     market_type = "spot"
+  }
+
+  vpc_security_group_ids = [aws_security_group.ssh_admin.id]
+  key_name               = aws_key_pair.admin_ssh_key.id
+
+  user_data = filebase64("${path.module}/../../setup-miner.sh")
+}
+
+resource "aws_instance" "miner" {
+  count = 0
+  launch_template  {
+    id = aws_launch_template.miner["x86_64"].id
+  }
+  associate_public_ip_address = "true"
+
+  instance_market_options {
     spot_options {
       max_price = 0.7
     }
   }
-
-  vpc_security_group_ids      = [aws_security_group.ssh_admin.id]
-  key_name                    = aws_key_pair.admin_ssh_key.id
-  associate_public_ip_address = "true"
-
-  user_data = file("${path.module}/../../setup-miner.sh")
 }
