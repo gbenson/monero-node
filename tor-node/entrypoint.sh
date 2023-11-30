157abd189c0232a8d32e273a6dd6149fcbb4c040
Gary Benson <gary@gbenson.net>
2023-11-30 20:57:31 +0000

    Don't fail on container restart

--- tor-node/entrypoint.sh
+++ tor-node/entrypoint.sh
@@ -16,15 +16,31 @@ fi
 
 if [ -n "$port" ]; then
   f=/etc/tor/torrc
-  grep ^HiddenServicePort $f && exit 1  # crowbar
-  sed -e '0,/^#HiddenServicePort/{s/^#\(HiddenService\)/\1/}' \
-      -e '/^HiddenServicePort/{s/80/@@PORT@@/g}' \
-      -i $f
-  if [ -n "$host" ]; then
-    sed -i "/^HiddenServicePort/{s/127\.0\.0\.1/@@HOST@@/}" $f
-  fi
-  sed -e "/^HiddenServicePort/{s/@@HOST@@/$host/; s/@@PORT@@/$port/g}" \
-      -i $f
+
+  ts=$(date "+%b %d %H:%M:%S.%N" | cut -c -19)
+  echo -n "$ts [notice] Checking \"$f\"... "
+  for loop in 1 2; do
+    if grep "^HiddenServicePort $port ${host:-127.0.0.1}:$port\$" $f; then
+      break
+
+    elif grep ^HiddenServicePort $f; then
+      ts=$(date "+%b %d %H:%M:%S.%N" | cut -c -19)
+      echo "$ts [error] \"$f\" isn't right, not starting Tor"
+      exit 1
+    fi
+
+    echo -n "configuring... "
+
+    sed -e '0,/^#HiddenServicePort/{s/^#\(HiddenService\)/\1/}' \
+	-e '/^HiddenServicePort/{s/80/@@PORT@@/g}' \
+	-i $f
+    if [ -n "$host" ]; then
+      sed -i "/^HiddenServicePort/{s/127\.0\.0\.1/@@HOST@@/}" $f
+    fi
+    sed -e "/^HiddenServicePort/{s/@@HOST@@/$host/; s/@@PORT@@/$port/g}" \
+	-i $f
+  done
+
   unset f
 fi
 
